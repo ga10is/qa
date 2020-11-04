98561102aef61d2d9d3ff3e1adf7ef7ec77cfe85
ga10is <ga10is1831@gmail.com>
2020-11-04 15:15:47 +0900

    get subclause

--- qa/unqg/mask_text.py
+++ qa/unqg/mask_text.py
@@ -7,6 +7,7 @@ from functools import wraps
 import time
 from multiprocessing import Pool
 import itertools
+from collections import deque
 
 from .generate_clozes import named_entity_answer_generator, noun_phrase_answer_generator, \
     is_appropriate_cloze, is_appropriate_answer, mask_answer
@@ -32,12 +33,15 @@ def mask_text(args):
 
     subline_gen = subline_generator(args.input_file, 1000)
     for i, sublines in enumerate(subline_gen):
+        print('Processing text...')
         sublines = [processed for processed in preprocess_generator(sublines)]
-        masked_sents = list(mask_texts_generator(sublines,
+        print('Dividing text to subclause...')
+        subclauses = [
+            subclause for subclause in subclauses_generator_from_texts(sublines, nlp)]
+        masked_sents = list(mask_texts_generator(subclauses,
                                                  nlp,
                                                  answer_generator,
                                                  n_process=-1))
-        # masked_sents = mask_texts_to_cloze(sublines, answer_generator, 8)
         with open(args.output_file, 'a', encoding='utf-8') as f:
             f.writelines('\n'.join(masked_sents))
 
@@ -64,9 +68,75 @@ def preprocess_generator(texts):
         text = text.rstrip()
         if text == '':
             continue
+        if contains_diacritical_mark(text):
+            # skip text which contains diacritical marks
+            continue
         text = unicodedata.normalize('NFKC', text)
         text = re.sub(r'(\d+年)\(.*?\d+年\)', '\\1', text)
         yield text
+
+
+def contains_diacritical_mark(text):
+    diacritaical_marks = ['\u00B4', '\u00A8', '\u203E',
+                          '\u00AF', '\uFFE3', '\u00B8', '\u309B', '\u309C']
+    for mark in diacritaical_marks:
+        if mark in text:
+            return True
+    return False
+
+#
+# Subclause
+#
+
+
+def subclauses_generator_from_texts(texts, nlp):
+    for text in texts:
+        for subclause in subclauses_generator(text, nlp):
+            yield subclause
+
+
+def subclauses_generator(text, nlp):
+    doc = nlp(text)
+    for tokens in doc.sents:
+        exclude_indices = []
+        for token in tokens:
+            # 1つもadvclがないときの対応
+            if token.dep_ == 'advcl' and token.pos_ != 'AUX':
+                subclause, indices = get_subtree(
+                    tokens, token.i, exclude_indices)
+                exclude_indices.extend(indices)
+                yield subclause
+        main_clause = ''.join([token.orth_ for token in tokens
+                               if token.i not in exclude_indices])
+        if main_clause != '':
+            yield main_clause
+
+
+def get_subtree(tokens, subtree_root_idx, exclude):
+    """
+    Returns
+    -------
+    subcluase text
+        str
+    """
+    indices = []
+    queue = deque()
+    queue.append(subtree_root_idx)
+    while len(queue) > 0:
+        head_idx = queue.popleft()
+        for token in tokens:
+            if token.i in exclude:
+                continue
+            if token.head.i == head_idx:
+                indices.append(token.i)
+                exclude.append(token.i)
+                queue.append(token.i)
+            elif token.i == head_idx:
+                indices.append(token.i)
+
+    indices.sort()
+    subclause = ''.join([t.orth_ for t in tokens if t.i in indices])
+    return subclause, indices
 
 
 def count_lines(path, blocksize=65536):
@@ -81,6 +151,10 @@ def count_lines(path, blocksize=65536):
     with open(path, 'r') as f:
         return sum(bl.count('\n') for bl in blocks(f))
 
+#
+# Mask text
+#
+
 
 def mask_texts_generator(texts, nlp, answer_generator, n_process):
     for doc in nlp.pipe(texts,
@@ -88,7 +162,7 @@ def mask_texts_generator(texts, nlp, answer_generator, n_process):
                         n_process=n_process,
                         batch_size=100):
         for sentence in doc.sents:
-            if is_appropriate_cloze(sentence.text):
+            if is_appropriate_cloze(sentence):
                 answers = answer_generator(sentence)
                 for answer_text, answer_start, answer_type in answers:
                     if is_appropriate_answer(answer_text):
